aeb7caea1f525a9e7f4c7ec5bce98a006e2a51a9
Yasir-Ullah <yasirjsdev@gmail.com>
2026-06-23 13:42:23 +0500

    Delete dark-mode directory

--- dark-mode/jscipt.js
+++
@@ -1,7 +0,0 @@
-let icon = document.querySelector("#icon");
-icon.addEventListener("click", function () {
-  document.body.classList.toggle("dark-them");
-  if (document.body.classList.contains("dark-them")) {
-    icon.src = "./images/sun.png";
-  } else [(icon.src = "./images/moon.png")];
-});
